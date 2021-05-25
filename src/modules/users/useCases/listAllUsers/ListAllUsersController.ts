17cf1245346baccccc360594a8473a880918d9fd
arthurgmr <arthur.gmr@hotmail.com>
2021-05-24 22:06:43 -0300

    :white_check_mark: insert user model

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,19 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      console.log(users);
+      return response.json(users);
+    } catch (error) {
+      console.log(error);
+      return response.status(400).json({ error: "User not is admin!" });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,10 +3,18 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const userUpdated = this.turnUserAdminUseCase.execute({ user_id });
+      return response.json(userUpdated);
+    } catch (error) {
+      console.log(error);
+      return response.status(404).json({ error: "User not found" });
+    }
   }
 }
 
